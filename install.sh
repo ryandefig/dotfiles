95e1642c95bc2339b1d1b50c050f2851e03969f4
Ryan DeFigueiredo <dev@ryandefig.com>
2023-01-25 22:12:27 -0500

    Removes homebrew installation from install script and fixes install bugs

--- install.sh
+++ install.sh
@@ -1,41 +1,28 @@
 #!/usr/bin/env sh
 
+if [[ "./install.sh" != $0 ]]; then
+    echo "\n[ERROR] This script must be run from the dotfiles root directory.\n"
+    exit 1
+fi
+
 # Setup
 DOTFILES_ROOT=~0 # The current directory is the local repository root
 
-# Install homebrew
-echo "\nChecking for homebrew..."
-if which brew | grep -q 'not found'; then
-    echo "Homebrew (required for installation) is not installed. Install now?"
-    while true; do
-        read -p "[y/n]: " yn
-        case $yn in
-            [Yy] ) echo "Installing homebrew..."
-                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-                break
-                ;;
-            [Nn] ) echo "Aborting installation."
-                exit
-                ;;
-        esac
-    done
+# Set default shell to zsh (homebrew)
+echo "\nVerifying zsh installation..."
+ZSH_INSTALL="/usr/local/bin/zsh"
+if [[ -f $ZSH_INSTALL ]]; then
+    echo "Zsh (homebrew) is already installed."
 else
-    echo "Homebrew already installed."
-fi
-
-# Install zsh
-echo "\nChecking zsh installation..."
-if cat /etc/shells | grep -q '/usr/local/bin/zsh'; then
-    echo "Zsh already installed."
-else
-    echo "Zsh not found. Installing zsh via homebrew..."
+    echo "Zsh (homebrew) is not installed. Installing via homebrew..."
     brew install zsh
 fi
 
-# Change default shell to homebrew-installed zsh
-echo "\nSetting default shell to zsh..."
-if [[ "/usr/local/bin/zsh" != $SHELL ]]; then
-    chsh -s /usr/local/bin/zsh
+if [[ $ZSH_INSTALL == $SHELL ]]; then
+    echo "Zsh (homebrew) is already the default shell."
+else
+    echo "Zsh (homebrew) is not the default shell. Making it the default shell..."
+    sudo sh -c "echo ${ZSH_INSTALL} >> /etc/shells && chsh -s ${ZSH_INSTALL}"
 fi
 echo "Done."
 
